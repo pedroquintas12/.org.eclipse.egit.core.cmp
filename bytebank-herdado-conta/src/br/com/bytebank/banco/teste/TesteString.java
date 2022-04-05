c32f0d8e31573495c5aa6b0b897f50b51fb6e8b0
Pedro Quintas <85141961+pedroquintas12@users.noreply.github.com>
2022-04-05 18:33:43 -0300

    Create TesteString.java

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,18 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String  nome ="pedro";
+		
+		nome.replace("P", "p");
+		
+		String outra= nome.toUpperCase ();
+		
+		System.out.println(nome);
+		System.out.println(outra);
+		
+	}
+	
+}
