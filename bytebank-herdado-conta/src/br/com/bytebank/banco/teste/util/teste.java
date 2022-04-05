ae36d4029e6634e348ecc42b59da65f1bb624524
Pedro Quintas <85141961+pedroquintas12@users.noreply.github.com>
2022-04-05 18:33:36 -0300

    Create teste.java

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/teste.java
@@ -0,0 +1,23 @@
+package br.com.bytebank.banco.teste.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class teste {
+
+	public static void main(String[] args) {
+		
+		int idade = 29;
+		
+		Integer idadeRef = Integer.valueOf(29);
+		
+		System.out.println(idadeRef.);
+		
+		int valor = idadeRef.intValue();
+		
+		
+		List numeros = new ArrayList();
+		
+	}
+	
+}
