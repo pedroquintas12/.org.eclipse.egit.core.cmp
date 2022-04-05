6d1a9c8317c781ad69aecacbc7614e9deaa5d725
Pedro Quintas <85141961+pedroquintas12@users.noreply.github.com>
2022-04-05 18:32:56 -0300

    Create EditorVideo.java

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,12 @@
+//gerente é um Funcionario, gerente herda a class Funcionario
+public class EditorVideo extends Funcionario {
+
+		public double getBonificacao() {
+		
+		return 150;
+ 	}
+	
+	
+	
+	
+}
